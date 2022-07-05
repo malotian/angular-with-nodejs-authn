8f431b112627b820ba8a9ae9a109c06f04e5a229
Harjinder Dhanjal <{ID}+{username}@users.noreply.github.com>
2022-07-05 12:31:56 -0600

    angular - basic componnet and routes

---
+++ core360/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { CustomerHomeComponent } from './components/customer-home/customer-home.component';
+import { HomeComponent } from './components/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminHomeComponent,
+    ContactUsComponent,
+    CustomerHomeComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
