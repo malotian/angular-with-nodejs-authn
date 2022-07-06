52b58f41ef2c48858166e8f890a12192ecc6be75
Harjinder Dhanjal <{ID}+{username}@users.noreply.github.com>
2022-07-06 12:50:53 -0600

    refactorized, simplifies

--- angular-webapp/src/app/app-routing.module.ts
+++ angular-webapp/src/app/app-routing.module.ts
@@ -1,28 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminHomeComponent } from './components/admin-home/admin-home.component';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { CustomerHomeComponent } from './components/customer-home/customer-home.component';
 import { ForbiddenComponent } from './components/forbidden/forbidden.component';
 import { HomeComponent } from './components/home/home.component';
 import { AuthGuardService as AuthGuard } from './services/auth.guard.service';
 
 const routes: Routes = [
-  {
-    path: 'admin',
-    component: AdminHomeComponent,
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'customer',
-    component: CustomerHomeComponent,
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'guest',
-    component: ContactUsComponent,
-    canActivate: [AuthGuard],
-  },
   {
     path: 'home',
     component: HomeComponent,
--- angular-webapp/src/app/app.module.ts
+++ angular-webapp/src/app/app.module.ts
@@ -4,18 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminHomeComponent } from './components/admin-home/admin-home.component';
-import { ContactUsComponent } from './components/contact-us/contact-us.component';
-import { CustomerHomeComponent } from './components/customer-home/customer-home.component';
 import { HomeComponent } from './components/home/home.component';
 import { ForbiddenComponent } from './components/forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminHomeComponent,
-    ContactUsComponent,
-    CustomerHomeComponent,
     HomeComponent,
     ForbiddenComponent
   ],
