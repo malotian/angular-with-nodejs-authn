2b359d7ac7e43339fac4564943a49951a7ebccf1
Harjinder Dhanjal <{ID}+{username}@users.noreply.github.com>
2022-07-05 23:11:25 -0600

    Auth Guard working finally

---
+++ core360/src/app/services/auth.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  public isAuthenticated(): Boolean {
+    let userData = localStorage.getItem('userInfo');
+    if (userData && JSON.parse(userData)) {
+      return true;
+    }
+    return false;
+  }
+
+  public setUserInfo(user: any) {
+    localStorage.setItem('userInfo', JSON.stringify(user));
+  }
+
+  public authenticate() {
+    console.log("authenticate");
+    return this.http.get('/authn-handler/authenticate');
+  }
+}
